246d07129f58d3830100ee1d78c8c75a082c5895
Rita Casiello <rituzka@gmail.com>
2022-08-19 16:42:12 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import operations as op
+
+x = 6
+y = 2
+
+print("Suma = ", op.sumar(x, y), ", Resta = ", op.restar(x, y), ", Multiplicación = ", op.multiplicar(x, y), ", Division = ", op.dividir(x, y))
